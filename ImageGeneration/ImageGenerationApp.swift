3e938106042a739852195b862e46ee11f54e36c0
Nimrat Khalsa <nimratkhalsa16@icloud.com>
2024-09-19 19:51:10 +0530

    Initial Commit

---
+++ ImageGeneration/ImageGenerationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageGenerationApp.swift
+//  ImageGeneration
+//
+//  Created by Nimrat Khalsa on 9/19/24.
+//
+
+import SwiftUI
+
+@main
+struct ImageGenerationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
